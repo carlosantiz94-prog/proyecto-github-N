7b22992ff4bf965f2cd3a087a445965277494211
Carlos Santis <carlosantiz94@gmail.com>
2025-10-23 22:43:15 -0500

    corregir logica en programacion1 y añadir notas

---
+++ programa1.cpp
@@ -0,0 +1,13 @@
+// <html>
+// <head>
+// <title>Programa 1</title>
+// </head>
+// <body>
+// <h1>Programa 1</h1>
+
+// <p>Este programa calcula el promedio de los datos que se ingresan.</p>
+
+// <p>Ingrese los datos y pulse el botón "Calcular".</p>
+
+// <p>El resultado se mostrará en la pantalla.</p>
+// <html>
